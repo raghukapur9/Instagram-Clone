82155067664a648fdf0bd46b8ec4c3d6b072dab0
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2022-02-28 17:24:34 +0530

    completed contract development

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -21,10 +21,24 @@ contract Decentragram {
     address payable author
   );
 
+  event ImageTipped (
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
   mapping (uint => Image) public images;
 
   // create post
   function uploadImage(string memory _imgHash,string memory _desc) public {
+    // Checks description is not empty
+    require(bytes(_desc).length > 0);
+
+    // Checks imageHash is not empty
+    require(bytes(_imgHash).length > 0);
+    require(msg.sender != address(0x0));
+  
     imageCount = imageCount + 1;
     images[imageCount] = Image(imageCount,_imgHash , _desc, 0, msg.sender);
 
@@ -32,4 +46,13 @@ contract Decentragram {
   }
 
   // tip posts
+  function tipImages(uint _id) public payable{
+    require(_id>0 && _id<=imageCount);
+    Image memory _image = images[_id];
+    address payable _author = _image.author;
+    _author.transfer(msg.value); 
+    _image.tipAmount  = _image.tipAmount + msg.value;
+    images[_id] = _image;
+    emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _image.author);
+  }
 }
